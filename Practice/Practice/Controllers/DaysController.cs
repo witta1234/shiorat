d2d8c841137d0220af627c36f59a563af93d0d89
shiorat <witta12345@gmail.com>
2018-08-02 02:21:33 +0600

    01.08.18 2:21

---
+++ Practice/Practice/Controllers/DaysController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practice.Models;
+
+namespace Practice.Controllers
+{
+    [Route("api/days")]
+    [ApiController]
+    public class DaysController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetDay()
+        {
+            return Ok(DayContext.Current.Days);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetOneDay(int id)
+        {
+            var DaytoReturn = DayContext.Current.Days.FirstOrDefault(c => c.Id == id);
+            if (DaytoReturn == null)
+            { return NotFound(); }
+            return Ok(DaytoReturn);
+        }
+        [HttpDelete("delete/{id}")]
+        [HttpPut("update/{id}")]        
+        [HttpPost("add/{id}")]
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Test();
+        }
+        
+    }
+}
---
+++ Practice/Practice/Models/DaysDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice.Models
+{
+    public class DaysDTO
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Target { get; set; }
+    }
+}
