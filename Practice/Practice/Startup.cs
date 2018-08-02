003a4a6249800085b6de28c30eafb7f916678910
shiorat <witta12345@gmail.com>
2018-08-02 08:45:17 +0600

    02.08.18 9:00 тест маршрутов

--- Practice/Practice/Startup.cs
+++ Practice/Practice/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,10 +16,10 @@ namespace Practice
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-          
+
+            services.AddRouting();
 
 
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,11 +35,39 @@ namespace Practice
             }
             app.UseStatusCodePages();
 
+            // определяем обработчик маршрута
+            var myRouteHandler = new RouteHandler(Handle);
+            // создаем маршрут, используя обработчик
+            var routeBuilder = new RouteBuilder(app, myRouteHandler);
+            // само определение маршрута - он должен соответствовать запросу {controller}/{action}
+            routeBuilder.MapRoute("default", "{controller}/{action}");
+            routeBuilder.MapGet("api/{controller}/{action}", async (context) =>
+            {
+                await context.Response.WriteAsync("Hello From MapGet!");
+            });
+            //// строим маршрут
+            app.UseRouter(routeBuilder.Build());
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
            
 
-           
-
+            // собственно обработчик маршрута
 
         }
+
+        private async Task Handle(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello ASP.NET Core!");
+        }
+
+
+
+
+
+
     }
+    
 }
