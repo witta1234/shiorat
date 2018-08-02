fa5ef3e0fc03c6ff0f31eff67699869ac3bac27c
shiorat <witta12345@gmail.com>
2018-08-02 10:00:39 +0600

    02.08.18 10:00 пересоздание проекта в целях работоспособоности

---
+++ NewPractike/NewPractike/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewPractike.Models;
+
+namespace NewPractike.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("api")]
+        //[HttpGet()]
+        public IActionResult GetDay()
+        {
+            return Ok(DayContext.Current.Days);
+        }
+
+        //[HttpGet("{id}")]
+        public IActionResult GetOneDay(int id)
+        {
+            var DaytoReturn = DayContext.Current.Days.FirstOrDefault(c => c.Id == id);
+            if (DaytoReturn == null)
+            { return NotFound(); }
+            return Ok(DaytoReturn);
+        }
+        //[HttpDelete("delete/{id}")]
+        //[HttpPut("update/{id}")]        
+        //[HttpPost("add/{id}")]
+
+        [Route("test")]
+        public IActionResult Test()
+        {
+            return Content("Hello");
+        }
+    }
+}
---
+++ NewPractike/NewPractike/DayContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NewPractike.Models;
+
+namespace NewPractike
+{
+    public class DayContext
+    {
+        public static DayContext Current { get; } = new DayContext();
+        public List<DaysDTO> Days { get; set; }
+        public DayContext()
+        {
+            Days = new List<DaysDTO>()
+            {
+                new DaysDTO()
+                {
+                    Id=1,
+            Date = new DateTime(2018,08,01),
+            Target = "Разработка проекта",
+
+        }
+            };
+        }
+    }
+}
